405817a428d25bc24211eba0efd5eb936eea8450
Ayyappajith <124246626+AYJ01@users.noreply.github.com>
2025-08-19 00:47:01 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   useEffect(() => {
     if (isSuccess && !needsEmailVerification && userData) {
       sessionStorage.setItem('user', JSON.stringify(userData));
-      navigate('/layout');
+      navigate('/layout/chats');
     }
   }, [isSuccess, needsEmailVerification, userData, navigate]);
 
