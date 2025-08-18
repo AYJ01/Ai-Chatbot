feccb5c0672ad59ca8e0d9b6bd01b49ab2633a6d
Ayyappajith <124246626+AYJ01@users.noreply.github.com>
2025-08-18 20:26:12 +0530

    Update Error404.jsx

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -9,7 +9,7 @@ const Error404 = () => {
             <h2 className="md:text-6xl text-4xl font-semibold text-gray-800">Page Not Found</h2>
             <p className="text-base mt-4 text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
             <div className="flex items-center gap-4 mt-6">
-                <button type="button" onClick={()=>location.href='/login'} className="bg-indigo-500 hover:bg-indigo-600 px-7 py-2.5 text-white rounded active:scale-95 transition-all">
+                <button type="button" onClick={()=>location.href='/'} className="bg-indigo-500 hover:bg-indigo-600 px-7 py-2.5 text-white rounded active:scale-95 transition-all">
                     Go back home 
                 </button>
                 <button type="button" className="group flex items-center gap-2 px-7 py-2.5 active:scale-95 transition">
