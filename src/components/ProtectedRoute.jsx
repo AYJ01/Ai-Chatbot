50b8f3723890e006a2078ca8f544e90c0cce2dca
Ayyappajith <124246626+AYJ01@users.noreply.github.com>
2025-08-19 07:03:17 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,13 +4,9 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   const storedUser = sessionStorage.getItem('user');
   const user = storedUser ? JSON.parse(storedUser) : null;
-
-  // Redirect to login if no user
   if (!user) {
     return <Navigate to="/" replace />;
   }
-
-  // Else render the children components
   return children;
 };
 
